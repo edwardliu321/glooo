94008dd6e104ecc705e6a7e02462778131ec702d
edward <edwardliu321@gmail.com>
2020-02-02 23:56:07 -0500

    fix infinite emits

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 const roomRouter = require('./routes/room');
-require('dotenv').config();
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-server.listen(80);
-
 const port = process.env.PORT || 5000;
 
+//config
 app.use(cors());
 app.use(express.json());
-
 app.use('/room', roomRouter);
-
 app.use(express.static(path.join(__dirname, '../frontend/scripts')));
 
 
@@ -28,26 +22,40 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
 
-app.get('/', (req,res) => {
+app.get('/:id', (req,res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "index.html"))
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+const server = app.listen(80, () => {
+    console.log('listening on port 80')
+});
 
+const io = require('socket.io')(server);
+
+
+let prev = {};
 io.on('connection', (socket) => {
+    let roomId = '';
+    
     socket.on('join', (data) => {
-        let { roomId } = data;
+        roomId = data.roomId;
+        if(!prev[roomId]) prev[roomId] = 0;
         socket.join(roomId);
-        console.log(roomId);
     });
     socket.on('pause', (data) => {
-        socket.broadcast.emit('pause');
-        console.log('pause');
+        if(prev[roomId] === 2) return;
+        prev[roomId] = 2
+        socket.to(roomId).emit('pause');
+        console.log('pause in ' + roomId);
     });
     socket.on('play', (data) => {
-        socket.broadcast.emit('play', data);
-        console.log('play');
+        if(prev[roomId] === 1) return;
+        prev[roomId] = 1;
+        //socket.broadcast.emit('play', data);
+        socket.to(roomId).emit('play', data);
+        console.log('play in ' + roomId);
     });
 });
