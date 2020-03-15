3f406cd9db7c525f2ed93ec6543d39c987da66f0
Dominik Buszowiecki <buszowid@mcmaster.ca>
2020-03-15 18:50:04 -0400

    Added createRoom and joinRoom methods

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,21 @@ import React, { useState, useRef, useEffect } from 'react';
 import classes from './Home.module.css'
 
 const Home = (props) => {
+    const createRoom = () => {
+        axios.post(config.serverEndpoint + '/room/create')
+            .then((res) => history.push('/video/' + res.data.id))
+            .catch(err => console.log(err))
+    }
+    const joinRoom = () => {
+        let id = roomId;
+        axios.post(config.serverEndpoint + '/room/checkid', {id})
+            .then(res => {
+                if (res.data.exists) history.push('/video/' + id);
+                else message.error('Invalid Room Id');
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div>
             <h1 className={classes.text}>Hello</h1>
