217d93206af07c78592914a149c7b597f22b4353
edward <edwardliu321@gmail.com>
2020-03-15 17:24:35 -0400

    added home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Player from './components/Player';
+import Home from './components/Home';
 import 'antd/dist/antd.css'
 
 const App = (props) => {
@@ -8,6 +9,7 @@ const App = (props) => {
     return (
         <Router>
             <Switch>
+                <Route path="/" component={Home} />
                 <Route path="/video/:roomId" component={Player} />
             </Switch>
         </Router>
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './Home.css'
+
+const Home = (props) => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+
+export default Home;
