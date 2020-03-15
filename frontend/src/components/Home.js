5a3fb41c413fa1dc86babdb935f51f4b266dee9f
Dominik Buszowiecki <buszowid@mcmaster.ca>
2020-03-15 19:38:32 -0400

    Added create/join room buttons to home page.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,31 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import classes from './Home.module.css'
+import config from '../config'
+import { Button, message, Modal, Input, Typography } from 'antd';
+
+const { Text } = Typography
+const axios = require('axios')
 
 const Home = (props) => {
+
+    const history = useHistory();
+    const [modalVisible, setModalVisible] = useState(false);
+    const [roomId, setRoomId] = useState("");
+    const [roomError, setRoomError] = useState(false);
+    
+    const openModal = () => {
+        setModalVisible(true);   
+    }
+    const closeModal = () => {
+        setModalVisible(false);
+    }
+    const onIDChange = (e) => {
+        console.log(e.target);
+        let text = e.target.value;
+        setRoomId(text);
+    }
+    
     const createRoom = () => {
         axios.post(config.serverEndpoint + '/room/create')
             .then((res) => history.push('/video/' + res.data.id))
@@ -12,14 +36,40 @@ const Home = (props) => {
         axios.post(config.serverEndpoint + '/room/checkid', {id})
             .then(res => {
                 if (res.data.exists) history.push('/video/' + id);
-                else message.error('Invalid Room Id');
+                else {
+                    setRoomError(true);
+                }
             })
             .catch(err => console.log(err))
     }
 
+
+    let roomErrorMessageHtml;
+    if(roomError) {
+        roomErrorMessageHtml = (
+        <Text type="danger">Invalid Room ID</Text>
+        );
+    }
+    
+
     return (
         <div>
-            <h1 className={classes.text}>Hello</h1>
+            <div>
+                <div className={classes.btnContainer}>
+                    <Button size="large" type="primary" onClick={createRoom}>Create Room</Button>
+                    <Button size="large" onClick={openModal}>Join Room</Button>
+                </div>
+            </div>
+            <Modal
+                    title={"Enter Room Id"}
+                    visible={modalVisible}
+                    onOk={joinRoom}
+                    okText={"Join"}
+                    onCancel={closeModal}
+                >
+                    <Input size="large" value={roomId} onChange={onIDChange} placeholder="Room Id"/>
+                    {roomErrorMessageHtml}
+            </Modal>
         </div>
     )
 }
