6ffaa1766f39d50f50bae42f5abc4aad25602f5b
Edward Liu <liuz150@mcmaster.ca>
2020-06-05 20:13:42 -0400

    Loading spinner on load

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -1,10 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
 import YouTube from 'react-youtube';
 import SocketIOClient from 'socket.io-client'
-import { message, Button, Row, Col, Input, Card, Comment, Form } from 'antd'
+import { message, Button, Row, Col, Input, Card, Comment, Form, Spin } from 'antd'
 import config from '../config';
-import { UserOutlined } from '@ant-design/icons';
-import If from '../helpers/If';
+import { UserOutlined, LoadingOutlined} from '@ant-design/icons';
 
 const opts = {
     width: '853',
@@ -28,10 +27,10 @@ const Player = (props) => {
     const [chatBoxText, setChatBoxText] = useState(null);
     const [currentVideoId, setCurrentVideoId] = useState(null);
     const [videoIdText, setVideoIdText] = useState(null);
+    const [loading, setLoading] = useState(true);
     let ref = useRef({ socket: null, player: null, userId: null, requireTime: true, isHost: false, actionQueue: {}, chatBottom: null });
     let { socket, player, actionQueue } = ref.current;
 
-    document.test = actionQueue;
     function pushAction(action) {
         actionQueue[action] = true;
     }
@@ -67,11 +66,11 @@ const Player = (props) => {
         setChatBoxText(null);
     }
 
-    function chatKeyDown(e){
-        if(e.which === 13 && !e.shiftKey){
-            e.preventDefault(); 
+    function chatKeyDown(e) {
+        if (e.which === 13 && !e.shiftKey) {
+            e.preventDefault();
             sendChat();
-        } 
+        }
     }
 
     //******** Socket Logic *********/
@@ -94,11 +93,6 @@ const Player = (props) => {
                 if (data.host) {
                     ref.current.isHost = true;
                     ref.current.requireTime = false;
-                    //pauseVideo();
-                    //if(data.videoId){
-                    //default
-                    //cueVideoById(data.videoId, false);
-                    //}
                 }
                 else {
                     if (data.videoId)
@@ -106,8 +100,9 @@ const Player = (props) => {
                     else
                         ref.current.requireTime = false;
                 }
-                //socket.emit()
                 message.success(`Joined room "${roomId}"`);
+
+                setLoading(false);
             });
         });
 
@@ -173,7 +168,7 @@ const Player = (props) => {
 
     const cueVideoById = (videoIdSearch, emit) => {
         console.log(videoIdSearch)
-        if (!videoIdSearch ) return;
+        if (!videoIdSearch) return;
         setVideoIdText(videoIdSearch);
         let videoId = parseVideoId(videoIdSearch);
         console.log(videoId);
@@ -183,7 +178,7 @@ const Player = (props) => {
         player.loadVideoById(videoId);
         setCurrentVideoId(videoId);
         if (emit) {
-            socket.emit('cuevideo', { videoId:videoIdSearch });
+            socket.emit('cuevideo', { videoId: videoIdSearch });
         }
     }
 
@@ -255,82 +250,82 @@ const Player = (props) => {
     // })
     return (
         <>
-            <Row>
-                <Col span={4} />
-                <Col span={15}>
-                    <Row style={{ marginTop: "70px" }}>
-                        <Col span={17}>
-                            <Input.Search
-                                placeholder="Search"
-                                value={videoIdText}
-                                onChange={(e) => setVideoIdText(e.target.value)}
-                                onSearch={() => cueVideoById(videoIdText, true)}
-                                size="large"
-                            />
-                        </Col>
-                        {/* <Col span={8} /> */}
-                    </Row>
-                    <div style={{ display: currentVideoId ? '' : 'none' }}>
-                        <Row style={{ marginTop: "50px" }}>
-                            <Col span={24}>
-                                <YouTube
-                                    opts={opts}
-                                    onReady={playerReady}
-                                    onStateChange={playerStateChanged}
+            <Spin spinning={loading} indicator={<LoadingOutlined style={{ fontSize: 50 }} spin />}>
+                <Row>
+                    <Col span={4} />
+                    <Col span={15}>
+                        <Row style={{ marginTop: "70px" }}>
+                            <Col span={17}>
+                                <Input.Search
+                                    placeholder="Search"
+                                    value={videoIdText}
+                                    onChange={(e) => setVideoIdText(e.target.value)}
+                                    onSearch={() => cueVideoById(videoIdText, true)}
+                                    size="large"
                                 />
                             </Col>
                         </Row>
-                        <Row>
-                            <Col span={24}>
-                                <UserOutlined /> {userList.length}
-                            </Col>
-                        </Row>
-                    </div>
-                </Col>
-                <Col span={5} >
-                    <Card title="Glooo Chat" extra={<a href="#">Users</a>} style={{ height: '100vh' }} >
-                        <div style={{ overflowY: 'scroll', height: '75vh' }} className="hideScroll">
-                            {
-                                chatList.map(c =>
-                                    <Comment
-                                        key={c.id}
-                                        author={c.author}
-                                        content={
-                                            <p>
-                                                {c.content}
-                                            </p>
-                                        }
+                        <div style={{ display: currentVideoId ? '' : 'none' }}>
+                            <Row style={{ marginTop: "50px" }}>
+                                <Col span={24}>
+                                    <YouTube
+                                        opts={opts}
+                                        onReady={playerReady}
+                                        onStateChange={playerStateChanged}
                                     />
-                                )
-                            }
-                            <br></br>
-                            <br></br>
-                            <br></br>
-                            <br></br>
-                            <br></br>
-                            <div ref={el => ref.current.chatBottom = el}>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={24}>
+                                    <UserOutlined /> {userList.length}
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                    <Col span={5} >
+                        <Card title="Glooo Chat" extra={<a href="#">Users</a>} style={{ height: '100vh' }} >
+                            <div style={{ overflowY: 'scroll', height: '75vh' }} className="hideScroll">
+                                {
+                                    chatList.map(c =>
+                                        <Comment
+                                            key={c.id}
+                                            author={c.author}
+                                            content={
+                                                <p>
+                                                    {c.content}
+                                                </p>
+                                            }
+                                        />
+                                    )
+                                }
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <div ref={el => ref.current.chatBottom = el}>
+                                </div>
                             </div>
-                        </div>
-                        <div style={{ marginRight: '20px' }}>
-                            <Form.Item>
-                                <Input.TextArea
-                                    onChange={e => setChatBoxText(e.target.value)}
-                                    value={chatBoxText}
-                                    placeholder={'Say something!'}
-                                    rows={2} maxLength={200} style={{ resize: 'none' }} 
-                                    onKeyDown={chatKeyDown}    
-                                />
-                            </Form.Item>
-                            <Form.Item>
-                                <Button onClick={(sendChat)} type="primary" style={{ float: 'right' }}>
-                                    Chat
+                            <div style={{ marginRight: '20px' }}>
+                                <Form.Item>
+                                    <Input.TextArea
+                                        onChange={e => setChatBoxText(e.target.value)}
+                                        value={chatBoxText}
+                                        placeholder={'Say something!'}
+                                        rows={2} maxLength={200} style={{ resize: 'none' }}
+                                        onKeyDown={chatKeyDown}
+                                    />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Button onClick={(sendChat)} type="primary" style={{ float: 'right' }}>
+                                        Chat
                             </Button>
-                            </Form.Item>
-                        </div>
-                    </Card>
-                </Col>
-            </Row>
-
+                                </Form.Item>
+                            </div>
+                        </Card>
+                    </Col>
+                </Row>
+            </Spin>
         </>
     )
 }
